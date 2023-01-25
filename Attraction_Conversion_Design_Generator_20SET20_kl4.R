cb486391196a5f68382a9bd46c77a134d7572b33
Kevin Lattery, PhD <37308820+klattery@users.noreply.github.com>
2023-01-24 19:34:18 -0600

    Update Attraction_Conversion_Design_Generator_20SET20_kl4.R

--- Attraction_Conversion_Design_Generator_20SET20_kl4.R
+++ Attraction_Conversion_Design_Generator_20SET20_kl4.R
@@ -174,7 +174,7 @@ conversion_function <- function(ntest, ntest_perver, ntest_comp, show_eachitem, 
   # Next step TAKES TIME
   if(shiny) progress$inc(.2, message = "1. Creating Design for Items Shown Each Version")
   if(!shiny) message("1. Creating Design for Items Shown Each Version")
-  multdesigns <- mclapply(1:designs_stage1, gen_design,design_in, ntest_perver, target_1, target_1_wt) 
+  multdesigns <- mclapply(1:designs_stage1, function(i) gen_design(design_in, ntest_perver, target_1, target_1_wt))
   fit_all <- sapply(multdesigns, compfit, target_1, target_1_wt) # fit of each member above
   design_testpick <- multdesigns[[which.min(fit_all)]]  # design of test items to show for each version (row)
   summary_stat <- t(design_testpick) %*% design_testpick # shows frequency (diagnol) and cross tab of items shown together
